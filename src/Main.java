40999b4eb3e5f97315637d4b7b912c45dc92d1e1
Yuliya Kurbatova <j53781551@gmail.com>
2020-12-15 22:06:52 +0300

    Main added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int transfer = 1_101;
+        int balance = 100;
+        int bonus;
+        if (transfer > 1_000) { bonus = transfer / 100 + transfer + balance;
+        }
+        else { bonus = transfer + balance;}
+        System.out.println(bonus);
+    }
+}
